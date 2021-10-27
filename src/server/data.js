5423fd37d1564e1298f3c4e0cecdd5aa41326eea
Ethan Fisher <fisherappstudios@gmail.com>
2021-10-27 11:06:09 -0400

    Ugh i hope this works

--- src/server/data.js
+++ src/server/data.js
@@ -10,6 +10,9 @@ const io = require('socket.io')(server, {
 const PORT = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, '/../../build')))
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, '/../../build/index.html'))
+})
 server.listen(PORT, () => {
   console.log('Server listening on port ' + PORT)
 })
